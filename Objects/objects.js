da7b9e2569f5b880a07ea294f7f302027a368dd2
jluna93 <112587107+jluna93@users.noreply.github.com>
2023-03-17 16:32:33 -0600

    Initiated Objects chapter

---
+++ Objects/objects.js
@@ -0,0 +1,9 @@
+function demo1(){
+    //To access to a property of a object, use the dot notation
+    let titleOfPage = document.title;
+    //To modify a property of a object, use the dot notation and assign it a value as
+    //you will do to a single variable
+    document.title = "Lol we change this title!!!";
+    
+
+}
