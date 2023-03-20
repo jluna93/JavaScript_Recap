a0df16fc2cb071f5df73750416b655193193f2da
jluna93 <112587107+jluna93@users.noreply.github.com>
2023-03-20 12:35:44 -0600

    added more style and arrays on Objects chapter

--- Objects/objects.js
+++ Objects/objects.js
@@ -7,3 +7,31 @@ function demo1(){
     
 
 }
+
+function demo2Arrays(){
+    //Two different ways to create/declare an array
+    let myArray1 =  new Array();
+    let myArray2 = [];
+
+    //Adding items to array
+    myArray1.push(1);
+    myArray1.push(2);
+    myArray1.push(3);
+
+    //Adding items to array using index
+    myArray2[0] = 1;
+    myArray2[1] = 2;
+    myArray2[2] = 3;
+
+    //Using Array.length in a for, to show the content of an array
+    console.log('Lenght of myArray1 = ' + myArray1.length + '; Content: ');
+    for(let i = 0; i < myArray1.length; i++) {
+        console.log('myArray1[' + i + '] = ' + myArray1[i]);
+    }
+
+    console.log('Lenght of myArray2 = ' + myArray2.length + '; Content: ');
+    for(let i = 0; i < myArray2.length; i++) {
+        console.log('myArray2[' + i + '] = ' + myArray2[i]);
+    }
+
+}
