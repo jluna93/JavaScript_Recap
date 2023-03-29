ca09689a813067c92a49797b42b87b6035b5fea7
jluna93 <112587107+jluna93@users.noreply.github.com>
2023-03-29 16:44:38 -0600

    events 2

--- Events/events.js
+++ Events/events.js
@@ -3,6 +3,16 @@ const displayMessage = (msg,msgContainer) => {
     document.getElementById(msgContainer).innerHTML = msg;
 };
 
+const appendMessage = (msg,msgContainer) => {
+    let currentMessage = document.getElementById(msgContainer).innerHTML;
+    document.getElementById(msgContainer).innerHTML = currentMessage + msg + "<hr>";
+};
+
+const clearContainer = (msgContainer) => {
+    document.getElementById(msgContainer).innerHTML = "";
+};
+
+
 const getValueFromInput = (id) => {
    return document.getElementById(id).value;
 };
@@ -31,11 +41,58 @@ const set_demoButton2 = () => {
     });
 };
 
+const set_demoButton3 = () => {
+    let demoButton3 = document.getElementById("demoButton3");
+
+    demoButton3.addEventListener("click", function(e){
+        let eventInfo = e;
+        let idOfButton = this.id;
+        console.log(e);
+        displayMessage("Event: " + eventInfo + " <hr> Id of Button clicked: " + idOfButton, "msgContainer4");
+    });
+};
+
+const set_demo4 = () => {
+
+    let demoButton4 = document.getElementById("demoButton4");
+    let demoContainer = document.getElementById("demoContainer");
+
+    demoButton4.addEventListener("click",function(e){
+        console.log(this.id + " clicked");
+        appendMessage(this.id + " clicked","msgContainer5");
+       
+    });
+
+    demoContainer.addEventListener("click",function(){
+        console.log(this.id + " clicked");
+        appendMessage(this.id + " clicked","msgContainer5");
+    });
+
+    document.addEventListener("click",function(){
+        console.log("document clicked");
+        appendMessage("document clicked","msgContainer5");
+    });
+
+    window.addEventListener("click",function(){
+        console.log("window clicked");
+        appendMessage("window clicked","msgContainer5");
+    });
+
+    document.getElementById("demoButton4Cleaner").addEventListener("click",function(e){
+        e.stopPropagation();
+        clearContainer("msgContainer5");
+    });
+};
+
+
 document.addEventListener("DOMContentLoaded", function(){
     //Adding events
     displayMessage("Hello World. Sending this message from a listener!!! Listener waits for DOM to be fully loaded and then shows this message as a response", "msgContainer");
     set_demoButton1();
     set_demoButton2();
+    set_demoButton3();
+    set_demo4();
 
 });
+
 
